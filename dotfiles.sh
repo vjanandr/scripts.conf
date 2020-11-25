8d8e7c343d89fabc8eec78ebcf460ce499f0a184
Vijay Rajamani <vijayr@arrcus.com>
2020-11-25 20:48:16 +0000

    tmux changes

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,5 +1,6 @@
 mkdir fastcd
 git clone https://github.com/frazenshtein/fastcd ~/fastcd
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp .bashrc ~/.bashrc
 cp .vimrc ~/.vimrc
