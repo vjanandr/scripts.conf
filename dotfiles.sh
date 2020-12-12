96b1916db381f3317c703ff6d23fb265fb1c5bec
Vijay Anand R <vjanandr85@gmail.com>
2020-12-12 20:01:48 +0530

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,7 @@ mkdir fastcd
 git clone https://github.com/frazenshtein/fastcd ~/fastcd
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone git://github.com/joelthelion/autojump.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install -y autojump
 
 cp .bashrc ~/.bashrc
