16bdfe5f44fb2ddb1a51c34005cf842d433b1c58
Vijay Anand R <vjanandr@yahoo.com>
2021-10-06 06:06:51 +0000

    fix branch

--- scripts/td4_daily.sh
+++ scripts/td4_daily.sh
@@ -4,9 +4,9 @@
 # Interactive search.
 # Usage: `ff` or `ff <folder>`.
 #
-BUILD_DIR=/home/aten/rel/arrcus_rel/
-BUILD_DIR_ROOT=/home/aten/rel/
-IMAGE_DIR=/home/aten/daily_images
+BUILD_DIR=~/rel/arrcus_rel/
+BUILD_DIR_ROOT=~/rel/
+IMAGE_DIR=~/daily_images
 DATE_DIR=$(date +"%d-%m-%Y")
 
 echo "BEGIN: $DATE_DIR" >> /tmp/daily_builds
@@ -14,7 +14,7 @@ echo "BEGIN: $DATE_DIR" >> /tmp/daily_builds
 mkdir -p $BUILD_DIR_ROOT
 echo "Remove old ws"
 cd $BUILD_DIR_ROOT
-rm -rf /home/aten/rel/*
+rm -rf ~/rel/*
 cd $BUILD_DIR_ROOT
 git clone git@github.com:Arrcus/arrcus_rel.git -b td4-dev-tfno
 cd $BUILD_DIR
@@ -43,5 +43,5 @@ git commit -m "$DATE_DIR"
 echo "Start make world"
 make world
 ssh vijayr@infra1 'mkdir ~/daily_images/td4-dev/'$DATE_DIR
-scp $BUILD_DIR/images/ONL-standalone.bcm.ARCOS-arrcus-stretch-ufi-td4*AMD64_INSTALLED_INSTALLER vijayr@infra1:~/daily_images/$DATE_DIR/td4-dev/
+scp $BUILD_DIR/images/ONL-standalone.bcm.ARCOS-arrcus-stretch-ufi-td4*AMD64_INSTALLED_INSTALLER vijayr@infra1:~/daily_images/td4-dev/$DATE_DIR
 echo "END: $DATE_DIR" >> /tmp/daily_builds
