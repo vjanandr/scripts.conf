3b3b024aa55984e73aa2ea30285fd87e93522331
Vijay Anand R <vjanandr@yahoo.com>
2022-09-23 04:57:27 +0000

    gopath

--- scripts/td4_daily_.sh
+++ scripts/td4_daily_.sh
@@ -21,15 +21,27 @@ cd $BUILD_DIR
 echo "Pull rel branch"
 git pull
 make init
+echo "Pull ONL-XC branch"
+cd $BUILD_DIR/ONL-xc
+git pull
+echo "Pull ONL-standalone branch"
+cd $BUILD_DIR/ONL-standalone
+git pull
+echo "Pull bcmsdk branch"
+cd $BUILD_DIR/packages/bcmsdk/
+git pull
+echo "Pull libnl3 branch"
+cd $BUILD_DIR/packages/libnl3/
+git pull
+echo "Pull arrcus-sw branch"
 cd $BUILD_DIR/packages/arrcus-sw
 git pull
-cd $BUILD_DIR/packages/bcmsdk
-git pull
+echo "Commit current changes"
 cd $BUILD_DIR/
 git add .
 git commit -m "$DATE_DIR"
 echo "Start make world"
-make sa
+make world
 ssh vijayr@infra1 'mkdir ~/daily_images/td4-dev/'$DATE_DIR
 scp $BUILD_DIR/images/ONL-standalone.bcm.ARCOS-arrcus-stretch-ufi-td4*AMD64_INSTALLED_INSTALLER vijayr@infra1:~/daily_images/td4-dev/$DATE_DIR
 echo "END: $DATE_DIR" >> /tmp/daily_builds
