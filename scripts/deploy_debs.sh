9d8599816f403675f9b676e47d02bd5a2b6c7f47
Vijay Anand R <vjanandr@yahoo.com>
2024-09-29 12:46:17 +0000

    no history share

--- scripts/deploy_debs.sh
+++ scripts/deploy_debs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+dpkg -i arrcus-sw-dev*.deb
+dpkg -i arrcus-tech-suppor*.deb
 dpkg -i arrcus-base*.deb
 dpkg -i arrcus-cp-api*.deb
-dpkg -i arrcus-lc-api*.deb
 dpkg -i arrcus-pltf-api*.deb
+dpkg -i arrcus-lc-api*.deb
+dpkg -i arrcus-lc-common*.deb
+dpkg -i arrcus-lc-bcm-sflow*.deb
 dpkg -i arrcus-wh*.deb
 dpkg -i arrcus-ppm-api*.deb
 dpkg -i arrcus-cp*.deb
-#dpkg -i arrcus-lc-common*.deb
 dpkg -i arrcus-lc-cp*.deb
-dpkg -i arrcus-lc*hw*.deb
+dpkg -i arrcus-lc-bcm-hw*.deb
 dpkg -i arrcus-init-scripts*.deb
-dpkg -i arrcus-pltf-agent*.deb
+#dpkg -i arrcus-pltf*.deb
 dpkg -i arrcus-arcos*.deb
