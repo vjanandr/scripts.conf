5de42a8dd096ba70437614ccfb68ff99cf284302
Vijay Anand R <vjanandr@yahoo.com>
2025-09-01 07:05:28 +0000

    copliot

--- nvim/lua/plugins/configs.lua
+++ nvim/lua/plugins/configs.lua
@@ -68,23 +68,54 @@ return {
         })
     end,
   },
-  {
-    "folke/trouble.nvim",
-    opts = { use_diagnostic_signs = true },
-    keys = {
-      -- disable conflicting <leader>c* keys
-      { "<leader>cs", false },
-      { "<leader>cS", false },  -- THIS is the one you still see
-      { "<leader>cl", false },
-      { "<leader>cL", false },
-      { "<leader>cd", false },
-      { "<leader>cD", false },
+   {
+     "folke/trouble.nvim",
+     opts = { use_diagnostic_signs = true },
+     keys = {
+       -- disable conflicting <leader>c* keys
+       { "<leader>cs", false },
+       { "<leader>cS", false },  -- THIS is the one you still see
+       { "<leader>cl", false },
+       { "<leader>cL", false },
+       { "<leader>cd", false },
+       { "<leader>cD", false },
 
-      -- (optional) put Trouble on <leader>t* instead
-      { "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
-      { "<leader>tt", "<cmd>Trouble toggle<cr>", desc = "Trouble: toggle" },
-      { "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", desc = "Trouble: diagnostics" },
-      { "<leader>tq", "<cmd>Trouble qflist toggle<cr>", desc = "Trouble: quickfix" },
-    },
+       -- (optional) put Trouble on <leader>t* instead
+       { "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+       { "<leader>tt", "<cmd>Trouble toggle<cr>", desc = "Trouble: toggle" },
+       { "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", desc = "Trouble: diagnostics" },
+       { "<leader>tq", "<cmd>Trouble qflist toggle<cr>", desc = "Trouble: quickfix" },
+     },
+   },
+   {
+     "akinsho/bufferline.nvim",
+     opts = {
+       options = {
+         numbers = "ordinal", -- Shows buffer numbers (1, 2, 3, etc.)
+         show_buffer_icons = true,
+         show_buffer_close_icons = false,
+         show_close_icon = false,
+         show_tab_indicators = true,
+         separator_style = "slant",
+       },
+      },
+  },
+  -- GitHub Copilot AI Code Completion
+  {
+    "github/copilot.vim",
+    event = "BufEnter",
+    config = function()
+      -- Enable Copilot by default
+      vim.g.copilot_enabled = true
+
+      -- Set up keybindings for Copilot
+      vim.keymap.set("i", "<C-g>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+      vim.keymap.set("i", "<C-j>", 'copilot#Next()', { expr = true, silent = true })
+      vim.keymap.set("i", "<C-k>", 'copilot#Previous()', { expr = true, silent = true })
+      vim.keymap.set("i", "<C-x>", 'copilot#Dismiss()', { expr = true, silent = true })
+
+      -- Manual trigger for completion
+      vim.keymap.set("i", "<C-Space>", 'copilot#Suggest()', { expr = true, silent = true })
+    end,
   },
 }
