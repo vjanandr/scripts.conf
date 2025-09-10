b0c7781128df802367dd68a058bfc887966bc4e6
Vijay Anand R <vjanandr@yahoo.com>
2025-09-10 10:43:55 +0000

    remove nvim

--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Alternative: Use Telescope for fuzzy function finding
-vim.keymap.set("n", "<leader>tf", ":Telescope treesitter<CR>", { desc = "Find functions with Telescope" })
